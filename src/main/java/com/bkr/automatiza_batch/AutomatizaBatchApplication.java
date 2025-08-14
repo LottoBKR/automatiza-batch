f01f0b13a26a2a94967dc33fdcd8eb3570518db5
LottoBKR <leonardobueras247@gmail.com>
2025-08-14 08:21:12 -0400

    Commit inicial

---
+++ src/main/java/com/bkr/automatiza_batch/AutomatizaBatchApplication.java
@@ -0,0 +1,13 @@
+package com.bkr.automatiza_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizaBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatizaBatchApplication.class, args);
+	}
+
+}
